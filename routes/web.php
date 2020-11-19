e70f9a92c694f60b11f8bfeaa8803df31f540d9f
Rebecca Mead <beckyleemead@gmail.com>
2020-11-19 14:25:14 -0500

    require authorization for update & delete functions

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ $router->get('/', function () use ($router) {
 $router->post('/register','UsersController@register');
 
 $router->get('/users', 'UsersController@index');
-$router->post('/users/update', 'UsersController@update');
-$router->post('/users/delete', 'UsersController@delete');
 $router->get('/users/{id}', 'UsersController@get');
+
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->post('/users/update', 'UsersController@update');
+    $router->post('/users/delete', 'UsersController@delete');
+});
+
+
