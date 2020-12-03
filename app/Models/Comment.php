43bb96493f6919e2ed9f793d342640777951d025
Rebecca Mead <beckyleemead@gmail.com>
2020-12-03 16:20:28 -0500

    new table: comments

---
+++ app/Models/Comment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'comments';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'parent_id', 'idea_id', 'user_id', 'text'
+    ];
+
+}
--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CommentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            IdeaSeeder::class,
-            CommentSeeder::class
-        ]);
+        \App\Models\Comment::factory()->count(20)->create();
     }
 }
